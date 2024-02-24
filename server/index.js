6fcab64c371912ad302d0b343928f51c11adb79a
Ful_Lenovo <miahful73@gmail.com>
2024-02-24 17:02:31 +0000

    server setup, need to test connection to MySQL

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
+require("dotenv").config();
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World from the server!");
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: "localhost",
+  user: process.env.DB_USER, // Use environment variable
+  password: process.env.DB_PASSWORD, // Use environment variable
+  database: process.env.DB_NAME, // Use environment variable
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Simple route to test the database connection
+app.get("/test-db", (req, res) => {
+  pool.query("SELECT 1 + 1 AS solution", (error, results) => {
+    if (error) throw error;
+    res.send(`The solution is: ${results[0].solution}`);
+  });
 });
 
 app.listen(port, () => {
